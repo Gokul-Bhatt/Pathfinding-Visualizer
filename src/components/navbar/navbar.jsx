d91a0a3bc298ccf4f7b99fd2d455ed856be73215
GOKUL CHANDRA BHATT <142083386+Gokul-Bhatt@users.noreply.github.com>
2026-03-22 19:40:20 +0530

    responsive

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,11 +4,9 @@ import "./navbar.css"
 export const navbar = () => {
   return (
     <div className="navbar">
-      <h1 className="topheading">Pathfinding Visualizer</h1>
-      <div className="dropdown-container">
-        
-      </div>
-      {/* <button type="submit" className="btn">FIND</button> */}
+      <h1 className="topheading">
+        Pathfinding Visualizer
+        </h1>
     </div>
   );
 };
